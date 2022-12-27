40229d89a5e3b9cc2e54fa9391e85831ff2277a2
Paul <mothcar@gmail.com>
2022-12-27 06:32:38 +0000

    save test

--- routes/index.js
+++ routes/index.js
@@ -5,36 +5,33 @@ const User = require('../models/User')
 /* GET home page. */
 router.get("/", async (req, res, next) => {
   try {
-    // const insert = req.query 
     const users = await User.find()
     console.log('request : ', users)
     res.json(users)
-    // const newUser = new User()
-    // newUser.email = 'mothcar@naver.com'
-    // newUser.name = 'mothcar'
-    // newUser.age = 49
-    // newUser.save()
-    // .then((user)=>{
-    //   console.log('data : ', user)
-    //   // res.json({
-    //   //   message: 'User created'
-    //   // })
-    //   // res.render("index", { title: user });
-      
-    // }).catch((err)=>{
-    //   res.json({
-    //     message:'Not created...'
-    //   })
-    // })
+    
   } catch(err) {
     res.status(500).json({message: err.message})
+  }
+});
 
+router.post("/save", async (req, res, next) => {
+  try {
+    const insert = req.body 
+    // console.log('Insert params : ',insert)
+
+    const newUser = new User()
+    newUser.email = insert.email
+    newUser.name = insert.name
+    newUser.age = insert.age
+    const user = await newUser.save()
+    if(user) res.json(user)
+    else res.json({message:'No user'})
+    // res.render("index", { title: user });
+  } catch(err) {
+    res.status(500).json({message: err.message})
   }
   
-  console.log("test...............");
-  // console.log("test...............11");
-  // console.log("data : ", data);
-  // res.render("index", { title: "Express11" });
+  console.log("save...............");
 });
 
 module.exports = router;
